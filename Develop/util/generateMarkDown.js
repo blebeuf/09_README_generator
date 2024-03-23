c368f962d08d39400c64e45168885f60a0b9e82a
blebeuf <bryanlebeuf@gmail.com>
2024-03-22 21:46:12 -0500

    populate and connect the readme.md info within .js files

--- Develop/util/generateMarkDown.js
+++ Develop/util/generateMarkDown.js
@@ -12,7 +12,53 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}${licenseBadge}
+
+  # <Your-Project-Title>
+
+  ## Description
+  
+ ${data.description}
+   
+  ## Table of Contents (Optional)
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [Features](#features)
+  - [How to Contribute](#how-to-contribute)
+  - [Test](#test)
+  - [Questions](#questions)
+
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+
+  ## Credits
+  
+  ${data.credits}
+  
+  ## License
+  
+ 
+  ## Badges
+  
+
+  ## Features
+  
+  ${data.features}
+  
+  ## How to Contribute
+  
+  ${data.contribute}
+  
+  ## Tests
+  
+  ${data.tests}
 
 `;
 }
--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer= require('inquirer');
+const generateMarkdown = require('./Develop/util/generateMarkDown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba - here I used the first 10 on the list for the choices
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of the project?'},
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe this project:'},
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide any installation instructions for this project:'},
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any usage information for this project:'},
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Please list any credits or collaborators for this project:'},
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Please selcet a license used, if any:',
+        choices: [
+            'N/A',
+            'Apache 2.0 License',
+            'Boost Software License 1.0',
+            'BSD 3-Clause License',
+            'BSD 2-Clause License',
+            'CC0',
+            'Attribution 4.0 International',
+            'Attribution-ShareAlike 4.0 International',
+            'Attribution-NonCommercial 4.0 International',
+            'Attribution-NoDerivates 4.0 International',] },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'Please list any special function or functions for this project:'},
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Describe how others may contribute to this project:'},
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide any additional information for testing this project:'},
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your Github username:'},
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address:'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("output.html", outputString, (err) =>
+        err ? console.error("File did not write") : console.log("README.MD has been written")
+    );
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
